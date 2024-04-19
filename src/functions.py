15918752fb941e1fc4a475b873e917980baedd3e
Konstantinos Triotis <kostastriotis@hotmail.com>
2024-04-19 10:03:28 +0000

    function request

---
+++ src/functions.py
@@ -0,0 +1,22 @@
+def getRandomWord():
+  # TODO: Implement this function.
+  # Read words from targets.json file and return a random word.
+  pass
+
+
+def dictionaryCheck(word: str):
+  # TODO: Implement this function.
+  # Check if the word is in the dictionary.json file.
+  # Return True or False.
+  pass
+
+
+def validateWord(target: str, word: str):
+  # TODO: Implement this function.
+  # Compare [target] and [word] and return a list of numbers.
+  # If the number is -1, the letter is not in the target word.
+  # If the number is 0, the letter is in the target word but not in the correct position.
+  # If the number is 1, the letter is in the target word and in the correct position.
+  # Lets say the target word is "apple" and the user enters "alpha"
+  # The function should return [1, 0, 1, -1, -1]
+  pass
