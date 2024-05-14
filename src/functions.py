593523f120f3fcdc9e17115712dd74073845c2bc
Giwrgospalaskas <152080827+Giwrgospalaskas@users.noreply.github.com>
2024-05-14 22:07:46 +0300

    Add files via upload

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,8 @@
 import json
 import random
 
+WORD_LENGTH = 5
+
 def getRandomWord():
   # TODO: Implement this function.
   # Read words from targets.json file and return a random word.
@@ -8,14 +10,14 @@ def getRandomWord():
   f = open('targets.json', 'r', encoding='utf-8')
   targets = json.load(f) #φορτώνει το json σαν λεξικό της python
   targets_size = len(targets)
-  index = random.randint(0, targets_size-1) #τυχαιος αριθμός εντός του λεξιλογίου
+  index = random.randint(0, targets_size) #τυχαιος αριθμός εντός του λεξιλογίου
   target = targets[index]
-  
-  while len(target)!=5: #εξασφαλίζεται οτι η λέξη είναι 5 χαρακτήρες
-    index = random.randint(0, targets_size-1)
+  while len(target)!=WORD_LENGTH: #εξασφαλίζεται οτι η λέξη είναι 5 χαρακτήρες
+    index = random.randint(0, targets_size)
     target = targets[index]
 
   target = target.upper()
+  print(target)
 
   return target
 
@@ -43,20 +45,47 @@ def validateWord(target: str, word: str):
   # If the number is 1, the letter is in the target word and in the correct position.
   # Lets say the target word is "apple" and the user enters "alpha"
   # The function should return [1, 0, 1, -1, -1]  
-  
-  #αφου το a στο τελος του alpha που εβαλε ο χρήστης υπάρχει στο apple, κανονικα ετσι οπως εχεις εξηγησει πάνω θα πρέπει να επιστρέφει [1, 0, 1, -1, 0]
-
   results = [-1,-1,-1,-1,-1]
-
-  for i in range(0,len(target)): 
-    for j in range(0,len(word)): # συγκρίνει ολα τα γράμματα των συναρτήσεων ένα ένα και αν ταυτίζονται κανει το αντιστοιχο results 0
-      if word[j]==target[i]:results[j]=0
-      
   
-  for i in range(0,5):           # συγκρίνει τα γράμματα των λέξεων στις ίδιες θέσεις και όταν ταυτίζονται κανει το results 1
-    if word[i]==target[i]:results[i]=1
+  target_ls = []               #list that contains the letters of the target word
+  word_ls= []                  #list that contains the letters of the entry word
+  index_of_same = []           #list that contains the index numbers of the same letters between the target list and the word list this will be usefull later when deleting letters from the lists
+  index_of_diff = []           #list that contains the index numbers of the different letters between the target and word list
 
+  for i in range(0,WORD_LENGTH):  
+    target_ls.append(target[i])
+    word_ls.append(word[i])
+    if word_ls[i] == target_ls[i]:   
+      results[i] = 1
+      index_of_same.append(i)
+    else:
+      index_of_diff.append(i)
 
+  if results == [1,1,1,1,1]:
+    return results
+  
+  
+  if index_of_same:
+    for j in range (0, len(index_of_same)):
+      word_ls.remove(word_ls[index_of_same[j]-j])
+      target_ls.remove(target_ls[index_of_same[j]-j])
+    for index in range (0,len(word_ls)):
+      if word_ls[index] in target_ls:
+        target_ls.remove(word_ls[index])
+        results[index_of_diff[index]] = 0
+    
+  
+  else:
+    for index in range (0,len(word_ls)):
+      if word_ls[index] in target_ls:
+        target_ls.remove(word_ls[index])
+        results[index] = 0
+  
+  print(results)     
+  
   return(results)
 
+if __name__ == "__main__":
 
+  word = getRandomWord()
+  validateWord(word, "ASEOF")   
