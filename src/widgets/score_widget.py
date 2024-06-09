210f5f6a554521a4788dbb76bc067c1c3f362aa3
GoodGuyChris <150680867+ChrisScal@users.noreply.github.com>
2024-06-09 19:38:12 +0300

    Score box container

--- src/widgets/score_widget.py
+++ src/widgets/score_widget.py
@@ -7,4 +7,16 @@ class UserScoreBox(ft.UserControl):
         
     def build(self):
         self.ScoreBox = ft.Text(text_align=ft.TextAlign.LEFT,size=25,value=f"Score: 0")
-        return self.ScoreBox
+        self.main = ft.Container(
+            content=self.ScoreBox,
+            alignment=ft.alignment.center,
+            bgcolor=ft.colors.SECONDARY_CONTAINER,
+            expand_loose=1,
+            width=300,
+            height=50,
+            margin=0,
+            border=ft.border.all(2, ft.colors.GREY),
+            border_radius=5,
+            scale = ft.transform.Scale(scale=1),
+            animate_scale= ft.animation.Animation(50, ft.AnimationCurve.BOUNCE_IN))
+        return self.main
